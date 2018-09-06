d1d940718208beae06223514fb615054f6de7cc4
vaterlangen <vaterlangen@devshm.de>
2018-09-06 22:28:51 +0200

    Update ews-oab-decoder.c
    added handler for EWS_PT_USER_CERTIFICATE and EWS_PT_USER_X509_CERTIFICATE types used by some servers

--- src/addressbook/ews-oab-decoder.c
+++ src/addressbook/ews-oab-decoder.c
@@ -258,6 +258,8 @@ static const struct prop_field_mapping {
 	{EWS_PT_THUMBNAIL_PHOTO, E_CONTACT_PHOTO, ews_populate_photo},
 	{EWS_PT_OFFICE_LOCATION, E_CONTACT_OFFICE, ews_populate_simple_string},
 	{EWS_PT_X509_CERT, E_CONTACT_X509_CERT, ews_populate_cert},
+	{EWS_PT_USER_CERTIFICATE, E_CONTACT_X509_CERT, ews_populate_cert},
+	{EWS_PT_USER_X509_CERTIFICATE, E_CONTACT_X509_CERT, ews_populate_cert},
 	{EWS_PT_SEND_RICH_INFO, E_CONTACT_WANTS_HTML, ews_populate_simple_string},
 };
 
